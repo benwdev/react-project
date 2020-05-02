e924f6ebc72c50849960e7d7f99b68c5a810a178
benwdev <bwhitfield82@gmail.com>
2020-05-02 16:35:17 +0100

    adding basic state example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi I'm a react app</h1>
-      </div>
-    );
+import Person from './Person/Person';
+
+const app = props => {
+  const [personsState, setPersonsState] = useState(
+    {
+      persons: [
+        { name: 'Max', age: 30 },
+        { name: 'Max2', age: 35 },
+        { name: 'Max3', age: 38 }
+      ],
+      otherState: "blah"
+    }
+  )
+
+  const switchNameHandler = () => {
+    //console.log('was clicked')
+    setPersonsState({
+      persons: [
+        { name: 'Maxamilian', age: 30 },
+        { name: 'Max2', age: 35 },
+        { name: 'Max3', age: 38 }
+      ] 
+    })
   }
+
+  return (
+    <div className="App">
+      <h1>Hi I'm a react app</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+    
+  
 }
 
-export default App;
+export default app;
